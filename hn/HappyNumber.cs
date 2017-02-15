abea872a0b05160f70a8c7e0a4faca532ad9b2e7
HP User <HP User@DESKTOP-R2D96RB>
2017-02-15 10:24:14 -0500

    Add project files.

---
+++ hn/HappyNumber.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hn
+{
+    public class HappyNumber
+    {
+        public int StartingNumber { get; private set; }
+        public IList<int> Series { get; private set; }
+
+        bool? isHappyBf;
+        public bool isHappy
+        {
+            get
+            {
+                if (!isHappyBf.HasValue)
+                {
+                    isHappyBf = isHappyImplementation();
+                }
+
+                return isHappyBf.Value;
+            }
+            private set
+            {
+                isHappyBf = value;
+            }
+        }
+
+        public HappyNumber(int startingNumber)
+        {
+            //https://en.wikipedia.org/wiki/Happy_number
+            StartingNumber = startingNumber;
+            Series = new List<int>();
+        }
+
+        bool isHappyImplementation()
+        {
+            //first run through
+            int iterationStartingNumber = StartingNumber;
+
+            do
+            {
+                //split starting number into digits
+                char[] digits = iterationStartingNumber.ToString().ToCharArray();
+                int[] digitsI = digits.Select(x => int.Parse(x.ToString())).ToArray<int>();
+
+                //get sum of square of digits
+                iterationStartingNumber = digitsI.Sum(x => (int)(Math.Pow((double)x, (double)2)));
+
+                //if 1 return true
+                if (1 == iterationStartingNumber)
+                {
+                    return true;
+                }
+
+                //if already present in series return false
+                if (Series.Contains(iterationStartingNumber))
+                {
+                    return false;
+                }
+                //else add to series and continue
+                else
+                {
+                    Series.Add(iterationStartingNumber);
+                }
+            } while (true);
+        }
+    }
+}
---
+++ hn/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0 || args[0] == "/standard")
+            {
+                for (int i=1;i<101;i++)
+                {
+                    HappyNumber hn = new HappyNumber(i);
+                    Console.WriteLine($"({i})=>{hn.isHappy}");
+                }
+
+                Console.ReadLine();
+            }
+            else if (args[0] == "/parallel")
+            {
+                //continue here tomorrow
+                //https://msdn.microsoft.com/en-us/library/dd460713(v=vs.110).aspx
+                Parallel.For(0, 101,
+                  index =>  {
+                      HappyNumber hn = new HappyNumber(index);
+                      Console.WriteLine($"({index})=>{hn.isHappy}");
+                  }
+                );
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
